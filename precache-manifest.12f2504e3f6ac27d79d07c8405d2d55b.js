773aefd499dc9596df4fae12ca45166145036613
reginamandasari <regina.mandasari@accenture.com>
2022-11-28 15:09:10 +0100

    Updates

---
+++ precache-manifest.12f2504e3f6ac27d79d07c8405d2d55b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc509aeaab2a6e6e7f3dc58544363c4",
+    "url": "/admin-catering-gh-pages/index.html"
+  },
+  {
+    "revision": "ec4ac50c07328a68eac6",
+    "url": "/admin-catering-gh-pages/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "76322ee997ff067adcb2",
+    "url": "/admin-catering-gh-pages/static/css/main.8a688658.chunk.css"
+  },
+  {
+    "revision": "ec4ac50c07328a68eac6",
+    "url": "/admin-catering-gh-pages/static/js/2.a87f698d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/admin-catering-gh-pages/static/js/2.a87f698d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76322ee997ff067adcb2",
+    "url": "/admin-catering-gh-pages/static/js/main.60704ddb.chunk.js"
+  },
+  {
+    "revision": "6f09be05f5f14af2842a",
+    "url": "/admin-catering-gh-pages/static/js/runtime-main.67a5c424.js"
+  },
+  {
+    "revision": "50edd3580656b0d6899b414ea2f2ef32",
+    "url": "/admin-catering-gh-pages/static/media/Chew and Cheer blue_11.50edd358.jpg"
+  },
+  {
+    "revision": "e0c5efe72d8771a4a770c4c6d6f67f73",
+    "url": "/admin-catering-gh-pages/static/media/Chew and Cheer blue_21.e0c5efe7.jpg"
+  },
+  {
+    "revision": "eae103e8c1ab51d578227d3005e13069",
+    "url": "/admin-catering-gh-pages/static/media/bgimage.eae103e8.jpeg"
+  }
+]);
